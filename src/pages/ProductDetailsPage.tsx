98301821ae0b8a2e8999cda061cda838b0f34db9
jdifek <givotkov11@gmail.com>
2025-05-20 10:56:15 +0300

    wwqwq

--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import Header from "../components/Header";
 import $api from "../api/http";
